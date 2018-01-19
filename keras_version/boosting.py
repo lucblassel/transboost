a261c5b2c4895391b46f08bc3792ec1f8920d3e6
romaingautronapt <Romain@Romains-MacBook-Air.local>
2018-01-19 12:08:12 +0100

    top layer changed

--- keras_version/boosting.py
+++ keras_version/boosting.py
@@ -112,9 +112,9 @@ def top_layer_builder(lr,num_of_classes):
     model.add(Flatten(input_shape=train_data.shape[1:]))
     model.add(Dense(256, activation='relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(1, activation='linear'))
-    model.compile(optimizer='rmsprop',loss='binary_crossentropy',metrics=['accuracy'])
-    #model.compile(optimizer = optimizers.Adam(lr=0.0001), loss='binary_crossentropy', metrics=['accuracy'])
+    model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
+    #model.compile(optimizer='rmsprop',loss='binary_crossentropy',metrics=['accuracy'])
+    model.compile(optimizer = optimizers.Adam(lr=0.0001), loss='binary_crossentropy', metrics=['accuracy'])
     return model
 
 def top_layer_trainer(top_model,top_model_weights_path,epochs,batch_size,trainNum,valNum,testNum,lr):
