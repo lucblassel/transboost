f67c10e6822ee9368cbf8529c166a960c4e47716
romaingautronapt <Romain@Romains-MacBook-Air.local>
2018-01-21 00:01:32 +0100

    fixing bug

--- keras_version/callbackBoosting.py
+++ keras_version/callbackBoosting.py
@@ -17,13 +17,14 @@ class callbackBoosting(keras.callbacks.Callback):
     def __init__(self,threshold,metric):
         super(callbackBoosting,self).__init__()
         self.threshold = threshold
+        self.metric = metric
 
     def on_train_end(self, logs={}):
         print('training ended')
         return
 
     def on_epoch_end(self, epoch, logs={}):
-        if logs.get(metric) >= self.threshold:
+        if logs.get(self.metric) >= self.threshold:
             self.model.stop_training = True
-            print('stopping training at accuracy = '+str(logs.get('val_acc')))
+            print('stopping training at accuracy = '+str(logs.get(self.metric)))
         return
