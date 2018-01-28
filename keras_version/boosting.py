d30435693481b8a67e0e4a22af07d66c4c387d53
luc blassel <luc.blassel@gmail.com>
2018-01-28 16:03:16 +0100

    debugging model saving

--- keras_version/boosting.py
+++ keras_version/boosting.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author: romaingautronapt
 # @Date:   2018-01-15 14:59:20
-# @Last modified by:   zlanderous
-# @Last modified time: 2018-01-17T14:13:05+01:00
+# @Last modified by:   Luc Blassel
+# @Last modified time: 2018-01-28T15:57:45+01:00
 """
 inspired by https://blog.keras.io/building-powerful-image-classification-models-using-very-little-data.html
 """
@@ -334,7 +334,7 @@ def booster(full_model,x_train,y_train,x_val,y_val,epochs,threshold,layerLimit,t
 
 	for time in range(times):
 
-		current_model_path = as.join(models_path,"model_"+str(c)+"h5")
+		current_model_path = os.join(models_path,"model_"+str(c)+"h5")
 
 		train_boost_indexes = np.random.choice(indexes,p=prob,size=train_length,replace=True)
 		x_train_boost = take(x_train,train_boost_indexes)
